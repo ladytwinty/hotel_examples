3fcaa09464c7e112a5361c81808ea37b806b762b
ladytwinty <ladytwinty@gmail.com>
2017-03-09 12:13:40 +0200

    created

---
+++ js/common.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+		nav: true,
+		loop: true,
+		margin: 20,
+		dots: false,
+        smartSpeed: 1000,
+        responsiveClass: true,
+		responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+	});
+
+
+
+
+});
